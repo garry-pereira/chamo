bdb35cbf9b405abb7b7afad9b91808605c0c2288
Garry Michael Pereira <garrymp12@gmail.com>
2024-02-01 21:31:11 -0600

    fa icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,11 +8,11 @@
 
 <section class="flex items-center justify-around mx-auto w-11/12 h-12">
   <div>
-    <a class="px-3 py-1" href="/">Home</a>
+    <a class="px-3 py-1" href="/"><i class="fa-solid fa-house"></i></a>
   </div>
   <nav class="flex">
-    <a class="px-3 py-1" href="/items">Items</a>
-    <a class="px-3 py-1" href="/login">Login</a>
+    <a class="px-3 py-1" href="/items"><i class="fa-solid fa-shop"></i></a>
+    <a class="px-3 py-1" href="/login"><i class="fa-solid fa-right-to-bracket"></i></a>
   </nav>
 </section>
 
